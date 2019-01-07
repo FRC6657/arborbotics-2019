f5e4970943445f5142ea33fb938901d7ed2fe133
Scott Thunderclaw <scottwegley@gmail.com>
2019-01-07 10:45:06 -0800

    Addition of Joystick related methods and the declaration of the Joystick.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,36 +7,33 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
-
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
-
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
-
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
-
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+    
+    private Joystick happyStick = new Joystick(RobotMap.driveControllerID);
+    public double getArcadeSpeed() {
+		double supaY = ((Joystick) happyStick).getY();
+		return -supaY*supaY*Math.signum(supaY) * RobotMap.joystickArcadeSpeedModifier;
+    }
+    
+    public double getArcadeRotation() {
+		double supaTwist = ((Joystick) happyStick).getTwist();
+		return supaTwist * RobotMap.joystickArcadeRotationModifier;
+	}
+	
+	public double getArcadeRoationThrottle() {
+		double supaTwist = ((Joystick) happyStick).getTwist();
+		return supaTwist;
+	}
+	
+	public double getScaledThrottle() {
+		double supaThrottle = ((Joystick) happyStick).getThrottle();
+		return (0.5 * supaThrottle) + 0.5;
+    
+}
 }
