c35ded0a81202935f2c8a39a53f8050a8ec1ca32
IShaLinI <mrsaltinecracker1@gmail.com>
2019-11-08 16:49:50 -0800

    Added Controll for pneumatics on Joystick

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -148,8 +148,9 @@ public class Robot extends TimedRobot {
     else if(driveStick.getRawButton(1) == false & driveStick.getRawButton(2) == true){Robot.lift.liftMotor.set(-Speeds.Lift);}
     else{Robot.lift.liftMotor.set(0);}
 
+    if(driveStick.getRawButton(5) == true & driveStick.getRawButton(3) == false){Robot.pneumatics.solForward();}
+    else if(driveStick.getRawButton(5) == false & driveStick.getRawButton(3) == true){Robot.pneumatics.solReverse();}
 
-   
   }
  
 
